44c7ab87c0713b739715e5f60b5b870443d05302
Arturo G.C <a.gascocompte@gmail.com>
2020-08-28 17:14:37 +0200

    feat: bug fix

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { AddBusinessWizardComponent } from './add-business-wizard/add-business-wizard.component';
 import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
 
@@ -14,6 +15,6 @@ import { LoginPage } from './login.page';
     LoginPageRoutingModule,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  declarations: [LoginPage],
+  declarations: [LoginPage, AddBusinessWizardComponent],
 })
 export class LoginPageModule {}

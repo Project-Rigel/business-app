430c64a717558acbe6d9d8280dfa441854b36af3
Arturo G.C <a.gascocompte@gmail.com>
2020-08-28 17:39:50 +0200

    feat: added slide components

---
+++ src/app/login/add-business-wizard/business-cif-slide/business-cif-slide.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business-cif-slide',
+  templateUrl: './business-cif-slide.component.html',
+  styleUrls: ['./business-cif-slide.component.scss'],
+})
+export class BusinessCifSlideComponent {}

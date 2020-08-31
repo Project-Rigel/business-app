3888e5cde548d966a87302b5698f3eafbfe0bad8
Arturo G.C <a.gascocompte@gmail.com>
2020-08-31 13:03:48 +0200

    feat: added functional business-name slide

--- src/app/login/add-business-wizard/business-name-slide/business-name-slide.component.ts
+++ src/app/login/add-business-wizard/business-name-slide/business-name-slide.component.ts
@@ -1,8 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { IonInput } from '@ionic/angular';
+import { ErrorToastService } from '../../../services/error-toast.service';
 
 @Component({
   selector: 'app-business-name-slide',
   templateUrl: './business-name-slide.component.html',
   styleUrls: ['./business-name-slide.component.scss'],
 })
-export class BusinessNameSlideComponent {}
+export class BusinessNameSlideComponent implements OnInit {
+  @Output() onBusinessNameChosen = new EventEmitter<string>();
+  @ViewChild('businessName') input: IonInput;
+  businessNameForm: FormGroup;
+  submitEnabled = true;
+  submitClicked = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastService: ErrorToastService,
+  ) {}
+
+  ngOnInit() {
+    this.businessNameForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+    });
+  }
+
+  async submitForm(value: any) {
+    this.submitClicked = true;
+    if (!this.businessNameForm.valid) {
+      await this.toastService.present({
+        message: 'Error Tiene que indicar un nombre',
+        color: 'danger',
+      });
+    } else {
+      this.submitEnabled = false;
+      this.submitClicked = false;
+      this.onBusinessNameChosen.emit(this.name.value);
+    }
+  }
+
+  get name() {
+    return this.businessNameForm.get('name');
+  }
+
+  isInputInvalid(control: AbstractControl) {
+    if (this.submitClicked && control.invalid) {
+      return true;
+    }
+    if (control.invalid && control.touched && this.submitClicked) {
+      return true;
+    }
+
+    if (control.invalid && control.touched && !this.submitClicked) {
+      return false;
+    }
+
+    return false;
+  }
+}

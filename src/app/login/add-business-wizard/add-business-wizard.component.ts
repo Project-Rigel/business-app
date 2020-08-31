85e7493b814adf09f18b95dac2ea472f1d2fbd7b
Arturo G.C <a.gascocompte@gmail.com>
2020-08-31 13:03:48 +0200

    feat: added functional business-name slide

--- src/app/login/add-business-wizard/add-business-wizard.component.ts
+++ src/app/login/add-business-wizard/add-business-wizard.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { IonSlides, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-business-wizard',
   templateUrl: './add-business-wizard.component.html',
   styleUrls: ['./add-business-wizard.component.scss'],
 })
-export class AddBusinessWizardComponent {
+export class AddBusinessWizardComponent implements AfterViewInit {
+  @ViewChild(IonSlides) ionSlides: IonSlides;
+  slideOpts: any = {};
+  businessName: string;
+
   constructor(private modalController: ModalController) {}
 
   //ngOnInit() {}
+
+  async ngAfterViewInit(): Promise<void> {
+    await this.ionSlides.lockSwipes(true);
+  }
+
+  async saveBusinessName(name) {
+    this.businessName = name;
+    await this.nextSlide();
+  }
+
+  async nextSlide() {
+    await this.ionSlides.lockSwipeToNext(false);
+    await this.ionSlides.slideNext(600);
+    await this.ionSlides.lockSwipes(false);
+    await this.ionSlides.lockSwipeToNext(true);
+  }
 
   async cancel() {
     await this.modalController.dismiss({ done: false });

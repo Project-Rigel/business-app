d8470ebd16f76b2456a700cd6e072190b0d6de6e
Arturo G.C <a.gascocompte@gmail.com>
2020-09-01 10:30:23 +0200

    feat: added business-address and phone slides

--- src/app/login/add-business-wizard/business-address-slide/business-address-slide.component.ts
+++ src/app/login/add-business-wizard/business-address-slide/business-address-slide.component.ts
@@ -1,8 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { IonInput } from '@ionic/angular';
+import { ErrorToastService } from '../../../services/error-toast.service';
 
 @Component({
   selector: 'app-business-address-slide',
   templateUrl: './business-address-slide.component.html',
   styleUrls: ['./business-address-slide.component.scss'],
 })
-export class BusinessAddressSlideComponent {}
+export class BusinessAddressSlideComponent implements OnInit {
+  @Output() onBusinessAddressChosen = new EventEmitter<string>();
+  @ViewChild('businessAddress') input: IonInput;
+  businessAddressForm: FormGroup;
+  submitEnabled = true;
+  submitClicked = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastService: ErrorToastService,
+  ) {}
+
+  ngOnInit() {
+    this.businessAddressForm = this.formBuilder.group({
+      address: ['', [Validators.required]],
+    });
+  }
+
+  async submitForm(value: any) {
+    this.submitClicked = true;
+    if (!this.businessAddressForm.valid) {
+      await this.toastService.present({
+        message: 'Error Tiene que indicar una dirección',
+        color: 'danger',
+      });
+    } else {
+      this.submitEnabled = false;
+      this.submitClicked = false;
+      this.onBusinessAddressChosen.emit(this.address.value);
+    }
+  }
+
+  get address() {
+    return this.businessAddressForm.get('address');
+  }
+
+  isInputInvalid(control: AbstractControl) {
+    if (this.submitClicked && control.invalid) {
+      return true;
+    }
+    if (control.invalid && control.touched && this.submitClicked) {
+      return true;
+    }
+
+    if (control.invalid && control.touched && !this.submitClicked) {
+      return false;
+    }
+
+    return false;
+  }
+}
--- src/app/login/add-business-wizard/business-phone-number-slide/business-phone-number-slide.component.ts
+++ src/app/login/add-business-wizard/business-phone-number-slide/business-phone-number-slide.component.ts
@@ -1,8 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { IonInput } from '@ionic/angular';
+import { ErrorToastService } from '../../../services/error-toast.service';
+import { PhoneValidatorService } from '../../../services/phone-validator.service';
 
 @Component({
   selector: 'app-business-phone-number-slide',
   templateUrl: './business-phone-number-slide.component.html',
   styleUrls: ['./business-phone-number-slide.component.scss'],
 })
-export class BusinessPhoneNumberSlideComponent {}
+export class BusinessPhoneNumberSlideComponent {
+  @Output() onBusinessPhoneNumberChosen = new EventEmitter<string>();
+  @ViewChild('businessPhone') input: IonInput;
+  businessPhoneForm: FormGroup;
+  submitEnabled = true;
+  submitClicked = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastService: ErrorToastService,
+  ) {}
+
+  ngOnInit() {
+    this.businessPhoneForm = this.formBuilder.group({
+      phone: [
+        '',
+        [Validators.required, PhoneValidatorService.validCountryPhone('ES')],
+      ],
+    });
+  }
+
+  async submitForm(value: any) {
+    this.submitClicked = true;
+    if (!this.businessPhoneForm.valid) {
+      await this.toastService.present({
+        message: 'Error Tiene que indicar un nº de teléfono válido',
+        color: 'danger',
+      });
+    } else {
+      this.submitEnabled = false;
+      this.submitClicked = false;
+      this.onBusinessPhoneNumberChosen.emit(this.phone.value);
+    }
+  }
+
+  get phone() {
+    return this.businessPhoneForm.get('phone');
+  }
+
+  isInputInvalid(control: AbstractControl) {
+    if (this.submitClicked && control.invalid) {
+      return true;
+    }
+    if (control.invalid && control.touched && this.submitClicked) {
+      return true;
+    }
+
+    if (control.invalid && control.touched && !this.submitClicked) {
+      return false;
+    }
+
+    return false;
+  }
+}

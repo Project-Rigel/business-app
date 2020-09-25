e4723ff93b4b3486917b6859e0ea4894992cb27f
Arturo Gascó Compte <38910658+agascocompte@users.noreply.github.com>
2020-09-25 13:39:13 +0200

    [GIK-128] Refactor agenda-details
    * feat: added and refactored agenda-header component
    
    * feat: added and refactored agenda-time-selector component
    
    * chore: refactored chip string to datetime into a fuction

--- src/app/agenda/agenda-details/agenda-details.page.ts
+++ src/app/agenda/agenda-details/agenda-details.page.ts
@@ -1,19 +1,13 @@
 import { DatePipe } from '@angular/common';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {
-  AnimationController,
-  IonDatetime,
-  ModalController,
-} from '@ionic/angular';
+import { AnimationController, ModalController } from '@ionic/angular';
 import { Animation } from '@ionic/core';
 import * as moment from 'moment';
 import { duration, Duration, Moment } from 'moment';
 import { Observable } from 'rxjs';
 import { switchMap, take } from 'rxjs/operators';
-import { DatePickerComponent } from '../../components/date-picker/date-picker.component';
 import { Agenda } from '../../interfaces/agenda';
-import { Appointment } from '../../interfaces/appointment';
 import { Customer } from '../../interfaces/customer';
 import { Product } from '../../interfaces/product';
 import { AgendaService } from '../../services/agenda.service';
@@ -23,13 +17,6 @@ import { AuthService } from '../../services/auth.service';
 import { LoaderService } from '../../services/loader.service';
 import { AddAppointmentWizardComponent } from '../add-appointment-wizard/add-appointment-wizard.component';
 
-interface DisplayItem {
-  appointment?: Appointment;
-  interval: Date;
-  appointmentHeight?: number;
-  pxFromLast?: number;
-}
-
 @Component({
   selector: 'app-agenda-details',
   templateUrl: './agenda-details.page.html',
@@ -38,14 +25,11 @@ interface DisplayItem {
 export class AgendaDetailsPage implements OnInit {
   closeCalendar: Animation;
   openCalendar: Animation;
-  closeConfirmApoointment: Animation;
-  openConfirmAppoinment: Animation;
   isCalendarOpen = true;
-  isConfirmAppointmentOpen = false;
-  display: DisplayItem[] = [];
-  @ViewChild(DatePickerComponent)
-  datePicker: DatePickerComponent;
   calendarButtonColor = 'primary';
+
+  appointmentGaps: string[] = [];
+
   agenda$: Observable<Agenda>;
   addingAppointment = false;
   addingAppointmentInfo: {
@@ -53,22 +37,17 @@ export class AgendaDetailsPage implements OnInit {
     customer: Customer;
     product: Product;
   };
-  appoinmentGaps: string[] = [];
-  startDate: Moment;
-  endDate: Moment;
-  interval: Duration;
-  dateTimeInitialValue: Moment;
   dateTimeValue = new Date();
   selectedStartTime = false;
   possibleAppointmentId: string;
   appointment;
-  exisitingAppointment = null;
-  dayAppointments: Appointment[] = [];
-  productDuration: Duration;
+  existingAppointment = null;
   businessId: string;
   agendaId: string;
 
-  @ViewChild(IonDatetime) dateTime: IonDatetime;
+  startDate: Moment;
+  endDate: Moment;
+  interval: Duration;
 
   constructor(
     private animationController: AnimationController,
@@ -83,15 +62,8 @@ export class AgendaDetailsPage implements OnInit {
     this.startDate = moment(new Date().setHours(7, 0, 0, 0));
     this.endDate = moment(new Date().setHours(23, 0, 0, 0));
     this.interval = duration(30, 'minutes');
-    this.dateTimeInitialValue = moment(
-      new Date().setHours(this.startDate.get('hours')),
-    );
     const value = this.route.snapshot.paramMap.get('id');
     this.agenda$ = this.agendaService.getAgendaById(value);
-
-    this.appointmentsService.appointments$.subscribe(data => {
-      this.dayAppointments = data;
-    });
 
     this.auth.user$.subscribe(user => {
       if (user) {
@@ -118,20 +90,6 @@ export class AgendaDetailsPage implements OnInit {
       .duration(400)
       .fromTo('height', '0px', '246px')
       .easing('ease-in-out');
-
-    this.closeConfirmApoointment = this.animationController
-      .create()
-      .addElement(document.getElementById('confirm-container'))
-      .duration(400)
-      .fromTo('height', '200px', '0px')
-      .easing('ease-in-out');
-
-    this.openConfirmAppoinment = this.animationController
-      .create()
-      .addElement(document.getElementById('confirm-container'))
-      .duration(400)
-      .fromTo('height', '0px', '200px')
-      .easing('ease-in-out');
   }
 
   onDateChange(event) {
@@ -141,7 +99,7 @@ export class AgendaDetailsPage implements OnInit {
     this.updateAppointments(event);
   }
 
-  async showCalendar() {
+  async showOrHideCalendar() {
     this.calendarButtonColor = this.isCalendarOpen ? 'medium' : 'primary';
     this.openCalendar.stop();
     this.closeCalendar.stop();
@@ -149,15 +107,6 @@ export class AgendaDetailsPage implements OnInit {
       ? await this.closeCalendar.play()
       : await this.openCalendar.play();
     this.isCalendarOpen = !this.isCalendarOpen;
-  }
-
-  async showConfirmApoointmentDialog() {
-    this.openConfirmAppoinment.stop();
-    this.closeConfirmApoointment.stop();
-    this.isConfirmAppointmentOpen
-      ? await this.closeConfirmApoointment.play()
-      : await this.openConfirmAppoinment.play();
-    this.isConfirmAppointmentOpen = !this.isConfirmAppointmentOpen;
   }
 
   async startAddAppointmentWizard() {
@@ -169,48 +118,30 @@ export class AgendaDetailsPage implements OnInit {
         daySelected: this.dateTimeValue,
       },
     });
-
     await modal.present();
     const { data } = await modal.onWillDismiss();
-
-    if (data) {
-      if (data.done) {
-        this.addingAppointment = true;
-        this.addingAppointmentInfo = data;
-
-        await this.showConfirmApoointmentDialog();
-        const intervals = this.addingAppointmentInfo.intervals;
-        for (const gap of intervals) {
-          const item = moment(gap.from).utc(true);
-          this.appoinmentGaps.push(item.format('HH:mm'));
-        }
-      }
+    if (data && data.done) {
+      this.getAndShowAppointmentGaps(data);
     }
   }
 
-  selectTime($event) {
-    this.updatePossibleAppointment($event);
+  getAndShowAppointmentGaps(possibleAppointmentInfo: any) {
+    this.addingAppointment = true;
+    this.addingAppointmentInfo = possibleAppointmentInfo;
+    const intervals = this.addingAppointmentInfo.intervals;
+    for (const gap of intervals) {
+      const item = moment(gap.from).utc(true);
+      this.appointmentGaps.push(item.format('HH:mm'));
+    }
+  }
+
+  async onSelectedTime($event) {
+    await this.formatDateTimeValueFromChipString($event);
+    await this.updatePossibleAppointment();
     this.selectedStartTime = !this.selectedStartTime;
-    this.showConfirmApoointmentDialog();
   }
 
-  async updatePossibleAppointment($event: any) {
-    let hour = parseInt($event.target.innerText.split(':')[0]);
-    let minutes = parseInt($event.target.innerText.split(':')[1]);
-    if (isNaN(hour)) {
-      hour = parseInt($event.target.parentNode.innerText.split(':')[0]);
-      minutes = parseInt($event.target.parentNode.innerText.split(':')[1]);
-    }
-
-    this.dateTimeValue.setHours(hour);
-    this.dateTimeValue.setMinutes(minutes);
-    this.dateTimeValue.setSeconds(0);
-    this.dateTimeValue.setMilliseconds(0);
-
-    if (!this.possibleAppointmentId) {
-      this.possibleAppointmentId = this.appointmentsService.getId();
-    }
-
+  async updatePossibleAppointment() {
     const productDuration = moment.duration(
       this.addingAppointmentInfo.product.duration,
       'minutes',
@@ -253,7 +184,6 @@ export class AgendaDetailsPage implements OnInit {
       '¿Quiere confirmar la cita con los siguiente datos?',
       message,
     );
-
     result.pipe(take(1)).subscribe(async res => {
       if (res) {
         await this.confirmNewAppointment(this.agendaId);
@@ -302,22 +232,19 @@ export class AgendaDetailsPage implements OnInit {
   cancelAddAppointment() {
     this.addingAppointment = false;
     this.addingAppointmentInfo = null;
-    this.appoinmentGaps = [];
+    this.appointmentGaps = [];
     if (this.appointment != null) {
       this.appointmentsService.cancelAppointment(this.appointment);
       this.appointment = null;
     }
-    if (this.exisitingAppointment != null) {
+    if (this.existingAppointment != null) {
       this.appointmentsService.restoreExistingAppointment(
-        this.exisitingAppointment,
+        this.existingAppointment,
       );
-      this.exisitingAppointment = null;
+      this.existingAppointment = null;
     }
-
-    if (this.selectedStartTime) {
+    if (this.selectedStartTime)
       this.selectedStartTime = !this.selectedStartTime;
-    }
-
     this.possibleAppointmentId = null;
   }
 
@@ -327,8 +254,26 @@ export class AgendaDetailsPage implements OnInit {
     this.selectedStartTime = !this.selectedStartTime;
     this.possibleAppointmentId = null;
     this.appointment = null;
-    this.exisitingAppointment = null;
-    this.appoinmentGaps = [];
+    this.existingAppointment = null;
+    this.appointmentGaps = [];
+  }
+
+  formatDateTimeValueFromChipString(event) {
+    let hour = parseInt(event.target.innerText.split(':')[0]);
+    let minutes = parseInt(event.target.innerText.split(':')[1]);
+    if (isNaN(hour)) {
+      hour = parseInt(event.target.parentNode.innerText.split(':')[0]);
+      minutes = parseInt(event.target.parentNode.innerText.split(':')[1]);
+    }
+
+    this.dateTimeValue.setHours(hour);
+    this.dateTimeValue.setMinutes(minutes);
+    this.dateTimeValue.setSeconds(0);
+    this.dateTimeValue.setMilliseconds(0);
+
+    if (!this.possibleAppointmentId) {
+      this.possibleAppointmentId = this.appointmentsService.getId();
+    }
   }
 
   capitalize(text: string) {

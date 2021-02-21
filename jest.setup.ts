2b4bab7f5648dc872118989d5a65888a60bb6b54
Eduardo Simón <edusimonpicon@gmail.com>
2021-02-21 19:59:00 +0100

    passing tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,10 +1,10 @@
+import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js/dist/zone-testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { configure } from '@testing-library/angular';
 import '@testing-library/jest-dom';
 import 'jest-preset-angular';
-import 'zone.js/dist/zone'; // Included with Angular CLI.
-import 'zone.js/dist/zone-testing';
 
 configure({
   defaultImports: [IonicModule, ReactiveFormsModule],
